9a6d60830ab2d5f6530d7619d05a733dbaddeb21
Irakli Gunia <irakli.gunia@gmail.com>
2019-04-15 16:25:00 +0400

    სატესტო 1

--- main.py
+++ main.py
@@ -1,17 +1,77 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
-class MainWindow(QtWidgets.QMainWindow):
-	def __init__(self):
-		super(MainWindow, self).__init__()
-		uic.loadUi('main_window.ui', self)
-		self.test_button.clicked.connect(self.test_button_clicked)
+import socket
+import asyncore
 
-	def test_button_clicked(self, test):
-		self.test_label.setText("Heloo")
+host = "10.0.0.16"
+port = 12345                # Reserve a port for your service.
 
-app = QtWidgets.QApplication(sys.argv)
-main_window = MainWindow()
-main_window.show()
-sys.exit(app.exec_())
+# from PyQt5 import QtCore, QtGui, QtWidgets, uic
+# class MainWindow(QtWidgets.QMainWindow):
+# 	def __init__(self):
+# 		super(MainWindow, self).__init__()
+# 		uic.loadUi('main_window.ui', self)
+# 		self.test_button.clicked.connect(self.test_button_clicked)
+
+# 	def test_button_clicked(self, test):
+# 		self.test_label.setText("Heloo")
+
+# app = QtWidgets.QApplication(sys.argv)
+# main_window = MainWindow()
+# main_window.show()
+# sys.exit(app.exec_())
+
+
+class IesMonitoringServer(asyncore.dispatcher):
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(5)
+
+    def handle_accept(self):
+    	print('handle_accept')
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair
+            print 'Incoming connection from %s' % repr(addr)
+            handler = ClientHandler(sock)
+
+
+class ClientHandler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+    	print('handle_read')
+        data = self.recv(8192)
+        if data:
+            self.send(data)
+
+
+
+
+ies_monitoring_server = IesMonitoringServer(host, port)
+asyncore.loop()
+
+
+
+
+
+
+
+
+
+
+
+
+s = socket.socket()         # Create a socket object
+# host = socket.gethostname() # Get local machine name
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print ('Got connection from', addr)
+   c.send(b'Thank you for connecting')
+   # c.close()                # Close the connection

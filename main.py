4effe9f2788fd60b93625c73977a6111e433db2d
Irakli Gunia <irakli.gunia@gmail.com>
2019-04-17 12:18:12 +0400

    პირველი ვარიანტი

--- main.py
+++ main.py
@@ -2,76 +2,66 @@
 # -*- coding: utf-8 -*-
 import sys
 import socket
-import asyncore
+import asyncio
+import json
 
 host = "10.0.0.16"
 port = 12345                # Reserve a port for your service.
+s = socket.socket()         # Create a socket object
 
 # from PyQt5 import QtCore, QtGui, QtWidgets, uic
 # class MainWindow(QtWidgets.QMainWindow):
-# 	def __init__(self):
-# 		super(MainWindow, self).__init__()
-# 		uic.loadUi('main_window.ui', self)
-# 		self.test_button.clicked.connect(self.test_button_clicked)
+#   def __init__(self):
+#       super(MainWindow, self).__init__()
+#       uic.loadUi('main_window.ui', self)
+#       self.test_button.clicked.connect(self.test_button_clicked)
 
-# 	def test_button_clicked(self, test):
-# 		self.test_label.setText("Heloo")
+#   def test_button_clicked(self, test):
+#       self.test_label.setText("Heloo")
 
 # app = QtWidgets.QApplication(sys.argv)
 # main_window = MainWindow()
 # main_window.show()
 # sys.exit(app.exec_())
 
+def start_listening():
+    print('start_listening')
+    s.bind((host, port))        # Bind to the port
+    s.listen(5)                 # Now wait for client connection.
+    print(type(s))
 
-class IesMonitoringServer(asyncore.dispatcher):
-    def __init__(self, host, port):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.set_reuse_addr()
-        self.bind((host, port))
-        self.listen(5)
+async def accept_connections():
+    print('accept_connections')
+    while True:
+       connection, addr = s.accept()     # Establish connection with client.
+       connection_hendler(connection, addr)
+     
+def connection_hendler(connection, addr):
+    print ('Got connection from', addr)
+    json_message = connection.recv(4096)
 
-    def handle_accept(self):
-    	print('handle_accept')
-        pair = self.accept()
-        if pair is not None:
-            sock, addr = pair
-            print 'Incoming connection from %s' % repr(addr)
-            handler = ClientHandler(sock)
+    # print(json_message.decode("utf-8"))
+    # a = json_message.decode("utf-8")
+    message = json.loads(json_message.decode("utf-8"))
 
+    print(message["message_type"])
+    print(message["text"])
 
-class ClientHandler(asyncore.dispatcher_with_send):
-
-    def handle_read(self):
-    	print('handle_read')
-        data = self.recv(8192)
-        if data:
-            self.send(data)
-
-
-
-
-ies_monitoring_server = IesMonitoringServer(host, port)
-asyncore.loop()
+    connection.send(b'Thank you for connecting')
+    # connection.close()
 
 
 
+async def main():
+    start_listening()       
+    accept_connections_task = loop.create_task(accept_connections())
+    await asyncio.wait([accept_connections_task])
 
-
-
-
-
-
-
-
-
-s = socket.socket()         # Create a socket object
-# host = socket.gethostname() # Get local machine name
-s.bind((host, port))        # Bind to the port
-
-s.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print ('Got connection from', addr)
-   c.send(b'Thank you for connecting')
-   # c.close()                # Close the connection
+if __name__ == "__main__":
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+    except Exception as e:
+        pass
+    finally:
+        loop.close()

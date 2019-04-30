0234db74cc272d5da429bdf7bb3e7ecb67f77076
Irakli Gunia <irakli.gunia@gmail.com>
2019-04-30 18:47:46 +0400

    დავამატეთ კომენტარები და გადავედით PEP8 სტანდარტზე

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ import threading
 import json
 import time
 
+# სერვერის ip მისამართი
 host = "10.0.0.16"
-port = 12345                # Reserve a port for your service.
-s = socket.socket()         # Create a socket object
+
+# სერვეროს პორტი, რომელზეც ვუსმენთ client-ების შეტყობინებებს
+port = 12345
+# socket - ებიექტის შექმნა         
+socket_object = socket.socket()
+
+# შეტყობინებების მაქსიმალური ზომა ბაიტებში
 buffer_size = 8192
-exit_flag = False
+
+# must_close გლობალური ცვლადის საშუალებით გაშვებული thread-ები ხვდებიან რო უნდა დაიხურონ
+must_close = False
 
 # from PyQt5 import QtCore, QtGui, QtWidgets, uic
 # class MainWindow(QtWidgets.QMainWindow):
@@ -27,65 +35,106 @@ exit_flag = False
 # main_window.show()
 # sys.exit(app.exec_())
 
-# ფუნქცია ხსნის პორტს და იწყებს მოსმენას
 def start_listening():
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((host, port))
-    s.listen(5)
+    """ ფუნქცია ხსნის პორტს და იწყებს მოსმენას """
 
-# ფუნქცია ელოდება client-ებს და ამყარებს კავშირს.
-# კავშირის დათანხმების შემდეგ იძახებს connection_hendler - ფუნქციას
+    # ვუთითებთ სოკეტის პარამეტრებს
+    socket_object.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # მოსმენის დაწყება
+    socket_object.bind((host, port))
+
+    # ვუთითებთ მაქსიმალურ კლიენტების რაოდენობას ვინც ელოდება კავშირის დამყარებაზე თანხმობას
+    socket_object.listen(10)
+
 def accept_connections():
+    """ ფუნქცია ელოდება client-ებს და ამყარებს კავშირს. 
+    კავშირის დათანხმების შემდეგ იძახებს connection_hendler - ფუნქციას """
+
     print('Ready for accept connections...')
+
     while True:
         try:
-            connection, addr = s.accept()
+            # თუ client-ი მზად არის კავშირის დასამყარებლად დავეთანხმოთ
+            connection, addr = socket_object.accept()
+
+            # თითოეულ დაკავშირებულ client-ისთვის შევქმნათ და გავუშვათ
+            # ცალკე thread-ი client_handler_thread ფუნქციის საშუალებით
             threading.Thread(target = client_handler_thread, args = (connection, addr)).start()
         except Exception as err:
             pass
-        if exit_flag:
+
+        # შევამოწმოთ თუ must_close არის True
+        if must_close:
+            # გამოვიდეთ ციკლიდან რაც გამოიწვევს Thread-ის დახურვას
             break
 
-# json ტექსტს აკონვერტირებს dictionary ტიპში
 def bytes_to_dictionary(json_text):
+    """ json ტექსტს აკონვერტირებს dictionary ტიპში """
+
     return json.loads(json_text.decode("utf-8"))
 
-    # return json.loads(json_text.decode("utf-8"))
-
-# client-თან კავშირის დამყარების შემდეგ ფუნქცია კითხულობს
-# მის შეტყობინებას
 def client_handler_thread(connection, addr):
+    """ client-თან კავშირის დამყარების შემდეგ ფუნქცია კითხულობს მის შეტყობინებას """
+
+    # გამოაქვს დაკავშირებული კლიენტის მისამართი
     print ('Got connection from', addr)
+
     while True:
+        # ციკლის შეჩერება 0.5 წამით
         time.sleep(0.5)
+
+        # წავიკითხოთ client-სგან გამოგზავნილი შეტყობინება 
         json_message = connection.recv(buffer_size)
+
+        # წაკითხული შეტყობინება თუ ცარიელია გამოვტოვოთ ციკლის მიმდინარე ბიჯი
         if not json_message.decode("utf-8"):
             continue
         else:
+            # წაკითხული შეტყობინება bytes ობიექტიდან გადავიყვანოთ dictionary ობიექტში
             message = bytes_to_dictionary(json_message)
-            # clien-ს გავუგზავნოთ მესიჯის id იმის პასუხად რომ შეტყობინება მივიღეთ
+
+            # წასაშლელია if-ის პირობა მარტო
             if message["message_type"] != "blockkk":
+                # clien-ს გავუგზავნოთ მესიჯის id იმის პასუხად რომ შეტყობინება მივიღეთ
                 connection.send(bytes(message["message_id"],"utf-8"))
+                # წასაშლელია
                 print("sending : " + message["message_id"])
+            # წაკითხული შეტყობინების მერე დავხუროთ კავშირი
             connection.close()
+
             print("Closed client connection", addr)
+
+            # გამოვიდეთ ციკლიდან რაც გულისხმობს client_handler_thread დასრულებას და შესაბამისი Tread-ის დახურვას
             break
 
 def command_listener():
-    global exit_flag
+    """ ფუნქცია კითხულობს მომხმარებლის ბრძანებებს """
+
+    global must_close
+
     while True:
+        # დაველოდოთ მომხმარებლის ბრძანებას
         command = input("")
+
+        # თუ მომხმარებლის მიერ შეყვანილი ბრძანება არის `exit` დავხუროთ პროგრამა და socket_object ობიექტი
         if command == "exit":
             print("Bye...")
-            s.shutdown(socket.SHUT_RDWR)
-            s.close()
-            exit_flag = True
+            socket_object.shutdown(socket.SHUT_RDWR)
+            socket_object.close()
+            must_close = True
             break
 
-# ფუნქცია რომელიც ეშვება პირველი პროგრამის ჩართვის დროს
 def main():
+    """ მთავარი ფუნქცია რომელიც ეშვება პირველი პროგრამის ჩართვის დროს """
+    
+    # გავხსნათ პორტი და დაველოდოთ client-ის დაკავშირებას
     start_listening()
+
+    # შევქმნათ thread-ი accept_connections ფუნქციის საშუალებით რომელიც ამყარებს კავშირს client-ებთან
     threading.Thread(target = accept_connections).start()
+
+    # დავიწყოთ მომხმარებლის ბრძანებების მოსმენა
     command_listener()
 
 if __name__ == "__main__":

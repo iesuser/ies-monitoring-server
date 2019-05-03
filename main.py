bffcc5e15495c6969e4632399e86e7fc37a8adf3
Irakli Gunia <irakli.gunia@gmail.com>
2019-05-03 18:58:28 +0400

    mysql ბაზასთან კავშირი მყარდება ყოველი მესიჯის ჩაწერის დროს, პაროლები აღარ იწერება სკრიპტში

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import socket
 import threading
 import json
 import time
+import pymysql
+import os
 
 # სერვერის ip მისამართი
-host = "10.0.0.16"
+server_ip = "10.0.0.16"
 
 # სერვეროს პორტი, რომელზეც ვუსმენთ client-ების შეტყობინებებს
 port = 12345
-# socket - ებიექტის შექმნა         
+# socket - ებიექტის შექმნა
 socket_object = socket.socket()
 
 # შეტყობინებების მაქსიმალური ზომა ბაიტებში
@@ -20,20 +22,28 @@ buffer_size = 8192
 # must_close გლობალური ცვლადის საშუალებით გაშვებული thread-ები ხვდებიან რო უნდა დაიხურონ
 must_close = False
 
-# from PyQt5 import QtCore, QtGui, QtWidgets, uic
-# class MainWindow(QtWidgets.QMainWindow):
-#   def __init__(self):
-#       super(MainWindow, self).__init__()
-#       uic.loadUi('main_window.ui', self)
-#       self.test_button.clicked.connect(self.test_button_clicked)
+# mysql სერვერის ip
+mysql_server_ip = "localhost"
 
-#   def test_button_clicked(self, test):
-#       self.test_label.setText("Heloo")
+# mysql სერვერის პორტი
+mysql_server_port = 3306
 
-# app = QtWidgets.QApplication(sys.argv)
-# main_window = MainWindow()
-# main_window.show()
-# sys.exit(app.exec_())
+# mysql სერვერის მონაცემთა ბაზის სახელი
+mysql_database_name = "ies_monitoring_server"
+
+# mysql სერვერის მომხარებელი (მოთავსებულია .bashrc ფაილში)
+mysql_server_user = os.environ.get('mysql_server_user')
+
+# mysql სერვერის მომხმარებლის პაროლი (მოთავსებულია .bashrc ფაილში)
+mysql_user_pass = os.environ.get('mysql_user_pass')
+
+
+def connection_close(connection):
+    """ხურავს (კავშირს სერვერთან) პარამეტრად გადაცემულ connection socket ობიექტს"""
+
+    connection.shutdown(socket.SHUT_RDWR)
+    connection.close()
+
 
 def start_listening():
     """ ფუნქცია ხსნის პორტს და იწყებს მოსმენას """
@@ -42,10 +52,11 @@ def start_listening():
     socket_object.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     # მოსმენის დაწყება
-    socket_object.bind((host, port))
+    socket_object.bind((server_ip, port))
 
     # ვუთითებთ მაქსიმალურ კლიენტების რაოდენობას ვინც ელოდება კავშირის დამყარებაზე თანხმობას
     socket_object.listen(10)
+
 
 def accept_connections():
     """ ფუნქცია ელოდება client-ებს და ამყარებს კავშირს. 
@@ -60,7 +71,7 @@ def accept_connections():
 
             # თითოეულ დაკავშირებულ client-ისთვის შევქმნათ და გავუშვათ
             # ცალკე thread-ი client_handler_thread ფუნქციის საშუალებით
-            threading.Thread(target = client_handler_thread, args = (connection, addr)).start()
+            threading.Thread(target=client_handler_thread, args=(connection, addr)).start()
         except Exception as err:
             pass
 
@@ -69,22 +80,65 @@ def accept_connections():
             # გამოვიდეთ ციკლიდან რაც გამოიწვევს Thread-ის დახურვას
             break
 
+
 def bytes_to_dictionary(json_text):
     """ json ტექსტს აკონვერტირებს dictionary ტიპში """
 
     return json.loads(json_text.decode("utf-8"))
 
+
+def insert_message_into_mysql(message):
+    """ მესიჯის ჩაწერა მონაცემთა ბაზაში """
+
+    # წავიკითხოთ შეტყობინების id
+    message_id = message["message_id"]
+
+    # წავიკითხოთ შეტყობინების დრო
+    sent_message_datetime = message["sent_message_datetime"]
+
+    # წავიკითხოთ შეტყობინების ტიპი
+    message_type = message["message_type"]
+
+    # წავიკითხოთ შეტყობინების ტექსტი
+    text = message["text"]
+
+    # წავიკითხოთ Client-ის ip მისამართი საიდანაც მოვიდა შეტყობინება
+    client_ip = message["client_ip"]
+
+    # წავიკითხოთ client-ის სკრიპტის სახელი საიდანაც მოვიდა შეტყობინება
+    client_script_name = message["client_script_name"]
+
+    insert_statement = "INSERT INTO `messages` \
+    (`message_id`, `sent_message_datetime`, `message_type`, `text`, `client_ip`, `client_script_name`) \
+    VALUES('{}', '{}', '{}', '{}', '{}', '{}')".format(message_id, sent_message_datetime, message_type,
+                                                       text, client_ip, client_script_name)
+
+    # mysql ბაზასთან კავშირის დამყარების ცდა
+    for i in range(3):
+        mysql_connection = connect_to_mysql()
+        if not mysql_connection:
+            print("ვერ დაუკავშირდა mysql ბაზას და არ ჩაიწერა შემდეგი მესიჯი ბაზაში:" + message_id)
+            continue
+        else:
+            cursor = mysql_connection.cursor()
+            cursor.execute(insert_statement)
+            mysql_connection.commit()
+            cursor.close()
+            mysql_connection.close()
+            break
+
+
 def client_handler_thread(connection, addr):
     """ client-თან კავშირის დამყარების შემდეგ ფუნქცია კითხულობს მის შეტყობინებას """
 
     # გამოაქვს დაკავშირებული კლიენტის მისამართი
-    print ('Got connection from', addr)
+    print('Got connection from', addr)
 
     while True:
         # ციკლის შეჩერება 0.5 წამით
         time.sleep(0.5)
 
-        # წავიკითხოთ client-სგან გამოგზავნილი შეტყობინება 
+        # წავიკითხოთ client-სგან გამოგზავნილი შეტყობინება
         json_message = connection.recv(buffer_size)
 
         # წაკითხული შეტყობინება თუ ცარიელია გამოვტოვოთ ციკლის მიმდინარე ბიჯი
@@ -94,10 +148,13 @@ def client_handler_thread(connection, addr):
             # წაკითხული შეტყობინება bytes ობიექტიდან გადავიყვანოთ dictionary ობიექტში
             message = bytes_to_dictionary(json_message)
 
+            # მესიჯის ჩაწერა მონაცემთა ბაზაში
+            insert_message_into_mysql(message)
+
             # წასაშლელია if-ის პირობა მარტო
             if message["message_type"] != "blockkk":
                 # clien-ს გავუგზავნოთ მესიჯის id იმის პასუხად რომ შეტყობინება მივიღეთ
-                connection.send(bytes(message["message_id"],"utf-8"))
+                connection.send(bytes(message["message_id"], "utf-8"))
                 # წასაშლელია
                 print("sending : " + message["message_id"])
             # წაკითხული შეტყობინების მერე დავხუროთ კავშირი
@@ -105,8 +162,9 @@ def client_handler_thread(connection, addr):
 
             print("Closed client connection", addr)
 
-            # გამოვიდეთ ციკლიდან რაც გულისხმობს client_handler_thread დასრულებას და შესაბამისი Tread-ის დახურვას
+            # გამოვიდეთ ციკლიდან რაც გულისხმობს client_handler_thread დასრულებას და შესაბამისი Thread-ის დახურვას
             break
+
 
 def command_listener():
     """ ფუნქცია კითხულობს მომხმარებლის ბრძანებებს """
@@ -119,23 +177,77 @@ def command_listener():
 
         # თუ მომხმარებლის მიერ შეყვანილი ბრძანება არის `exit` დავხუროთ პროგრამა და socket_object ობიექტი
         if command == "exit":
-            print("Bye...")
-            socket_object.shutdown(socket.SHUT_RDWR)
-            socket_object.close()
             must_close = True
+            print("Bye...")
+            connection_close(socket_object)
+            # time.sleep(3)
             break
+
+
+def connect_to_mysql():
+    """ ფუნქცია უკავშირდება Mysql სერვერს"""
+
+    try:
+        mysql_connection = pymysql.connect(mysql_server_ip,
+                                           mysql_server_user,
+                                           mysql_user_pass,
+                                           mysql_database_name,
+                                           port=mysql_server_port)
+        print("Connection to mysql database established")
+    except Exception as ex:
+        print(ex)
+        return False
+    return mysql_connection
+
 
 def main():
     """ მთავარი ფუნქცია რომელიც ეშვება პირველი პროგრამის ჩართვის დროს """
-    
+
+    # mysql - თან დაკავშირება
+    # global mysql_connection
+    # mysql_connection = connect_to_mysql()
+
+    # mysql - თან კავშირის არ ქონის შემთხვევაში იხურება პროგრამა
+    # if not mysql_connection:
+    #     print("Bye...")
+    #     sys.exit(1)
+
     # გავხსნათ პორტი და დაველოდოთ client-ის დაკავშირებას
     start_listening()
 
     # შევქმნათ thread-ი accept_connections ფუნქციის საშუალებით რომელიც ამყარებს კავშირს client-ებთან
-    threading.Thread(target = accept_connections).start()
+    threading.Thread(target=accept_connections).start()
 
     # დავიწყოთ მომხმარებლის ბრძანებების მოსმენა
-    command_listener()
+    threading.Thread(target=command_listener).start()
 
 if __name__ == "__main__":
     main()
+    # pass
+
+
+# def load_gui():
+#     from PyQt5 import QtCore, QtGui, QtWidgets, uic
+
+#     class MainWindow(QtWidgets.QMainWindow):
+#       def __init__(self):
+#           super(MainWindow, self).__init__()
+#           uic.loadUi('main_window.ui', self)
+#           self.test_button.clicked.connect(self.test_button_clicked)
+
+#       def test_button_clicked(self, test):
+#           self.test_label.setText("Hi")
+#     global app
+#     app = QtWidgets.QApplication(sys.argv)
+#     main_window = MainWindow()
+
+#     print(1)
+#     main_window.show()
+#     print(2)
+#     app.exec_()
+#     # app.processEvents()
+#     # sys.exit(app.exec_())
+
+#     print(3)
+
+#     print(4)
